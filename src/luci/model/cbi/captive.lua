e350889ec8123cf094b241943ced55049b8c14f7
z163330 <z3301413033@163.com>
2026-01-07 09:09:52 +0800

    change

--- src/luci/model/cbi/captive.lua
+++ src/luci/model/cbi/captive.lua
@@ -1,7 +1,7 @@
 local json = require "luci.jsonc"
 local fs   = require "nixio.fs"
 
-local f = SimpleForm("cp_codes", "兑换码管理","可批量生成、作废、延长时长")
+local f = SimpleForm("captive", "兑换码管理", "批量生成、作废、延长时长")
 local o = f:field(Value, "count", "生成数量", "")
 o.rmempty = false
 o.datatype = "uinteger"
@@ -12,12 +12,13 @@ local n = f:field(Value, "note", "备注", "")
 
 function f.handle(self, state, data)
   if state == FORM_VALID then
-    luci.sys.call("/usr/bin/cp_codegen.sh %d %d '%s'"
-                  % {data.count, data.mins, data.note or ""})
-    luci.http.redirect(luci.dispatcher.build_url("admin/services/cp_codes"))
+    os.execute("/usr/bin/codegen.sh %d %d '%s'"
+               % {data.count, data.mins, data.note or ""})
+    luci.http.redirect(luci.dispatcher.build_url("admin/services/captive"))
   end
 end
 
+-- 已生成码列表
 local db = json.parse(fs.readfile("/etc/captive/codes.json") or "{}")
 local tbody = ""
 for c,d in pairs(db) do
